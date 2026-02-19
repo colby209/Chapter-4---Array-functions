60a7062e09a96a41f9c58b54c5ee970a531f4ad1
colby209 <cwill209@aum.edu>
2026-02-18 19:34:27 -0600

    Add files via upload

---
+++ Check_palindrome.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char word[100];
+    cin >> word;
+
+    int length = 0;
+    while (word[length] != '\0') 
+    {
+        length++;
+    }
+
+    int left_index = 0;
+    int right_index = length - 1;
+
+    bool is_palindrome = true;
+
+    while (left_index < right_index) 
+    {
+        if (word[left_index] != word[right_index]) 
+        {
+            is_palindrome = false;
+            break;
+        }
+        left_index++;
+        right_index--;
+    }
+    if (is_palindrome) 
+    {
+        cout << "Palindrome" << endl;
+    }
+    else 
+    {
+        cout << "Not Palindrome" << endl;
+    }
+
+    return 0;
+}
